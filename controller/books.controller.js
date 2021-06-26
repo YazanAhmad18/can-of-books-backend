422620eeee19fa8a71d26ea5eb928995afb1950c
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-06-26 18:34:05 +0300

    edit

--- controller/books.controller.js
+++ controller/books.controller.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const userModel = require('../models/user.model');
+
+const { userModel } = require('../models/user.model');
 
 const getBooks = (request, response) => {
 
     const { email } = request.query;
-
-    userModel.find({ email: email }, (error, user) => {
+    userModel.find({ email }, (error, user) => {
         if (error) {
             response.send(error)
         } else {
@@ -15,4 +15,62 @@ const getBooks = (request, response) => {
     });
 }
 
-module.exports = getBooks;
+const creatBook = (request, response) => {
+    const { email, bookName , description , status , url } = request.body;  
+
+    userModel.findOne({ email: email }, (error, userData) => {  
+        if (error) {
+            response.send(error);
+        } else {
+            console.log(userData.books);
+            userData.books.push({ 
+                name: bookName,
+                description, 
+                status,
+                url
+            });
+            userData.save();
+            response.json(userData);
+        }
+    })
+}
+
+
+const updateBook = (request, response) => {
+    const index = request.params.book_idx;
+    const { email, bookName , description , status , url } = request.body;
+
+    userModel.findOne({ email: email }, (error, userData) => {
+        if (error) {
+            response.send(error);
+        } else {
+            userData.books.splice(index, 1, { 
+                name: bookName ,
+                description, 
+                status,
+                url
+            });
+            userData.save();
+            response.send(userData);
+        }
+    });
+}
+
+const deleteBook = (request, response) => {
+    const index = request.params.book_idx;
+    const { email }  = request.query;
+    console.log(request.query);
+
+    userModel.findOne({ email: email }, (error, userData) => {
+
+        if (error) {
+            response.send(error);
+        } else {      
+            userData.books.splice(index, 1);
+            userData.save();
+            response.send(userData);
+        }
+    });
+}
+
+module.exports = { getBooks, creatBook, updateBook, deleteBook };
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = require('express'); // require the express package
-const app = express(); // initialize your express app instance
+const express = require('express'); 
+const app = express(); 
 const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = process.env.PORT;
-const getBooks = require('./controller/books.controller');
-const cors = require('cors');
-
+const {getBooks, creatBook , updateBook, deleteBook} = require('./controller/books.controller');
+const {seedUserData} = require('./models/user.model');
+const cors = require('cors'); 
 
 app.use(cors());
 
+
+app.use(express.json()); 
 
 mongoose.connect('mongodb://localhost:27017/myFavoriteBooks',
 { useNewUrlParser: true, useUnifiedTopology: true }
@@ -19,7 +21,16 @@ app.get('/', (req, res) => {
     res.send('Your API Server is running now');
 });
 
-app.get('/books', getBooks );
+
+
+app.get('/books', getBooks );  
+
+app.post('/book' , creatBook); 
+
+app.put('/book/:book_idx' , updateBook);  
+
+app.delete('/book/:book_idx' , deleteBook);  
+
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
